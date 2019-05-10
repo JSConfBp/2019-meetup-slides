5e35926aae2eb76f53fc1d554bc50f6d1cfb1b23
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-05-10 15:16:26 +0200

    extract speakerlist

---
+++ presentation/speakers.js
@@ -0,0 +1,174 @@
+
+export const jsSpeakers = [
+	{
+	  name: 'Charlie Gerard',
+	  title: 'Weaving the web - Programming textile-based interactions in JavaScript',
+	  topics: '',
+	  image: require('../assets/speakers/charlie_gerard.jpg'),
+	},
+	{
+	  name: 'Rubén Sospedra',
+	  title: 'Mastering UIs with Finite State Machines',
+	  topics: '',
+	  image: require('../assets/speakers/ruben_sospedra.jpg'),
+	},
+	{
+	  name: 'Braden Moore',
+	  title: 'Deciphering Brainwaves with the Web Audio API',
+	  topics: '',
+	  image: require('../assets/speakers/braden_moore.jpg'),
+	},
+	{
+	  name: 'Mauricio Palma',
+	  title: 'Accessibility vs latest Web APIs. Can’t we just get along?',
+	  topics: '',
+	  image: require('../assets/speakers/mauricio_palma.jpg')
+	},
+	{
+	  name: 'Isa Silveira',
+	  title: 'Looking under the rug: the art of learning from failure',
+	  topics: '',
+	  image: require('../assets/speakers/isa_silveira.jpg'),
+	},
+	{
+	  name: 'Adam Giese',
+	  title: 'Composing music with composed functions',
+	  topics: '',
+	  image: require('../assets/speakers/adam_giese.jpg'),
+	},
+	{
+	  name: 'Rebecca Hill',
+	  title: 'Essential JavaScript debugging tools for the modern detective',
+	  topics: '',
+	  image: require('../assets/speakers/rebecca_hill.jpg'),
+	},
+	{
+	  name: 'Stephanie Nemeth',
+	  title: 'How not to read the room: Creating socially awkward wearables with machine learning & javascript',
+	  topics: '',
+	  image: require('../assets/speakers/stephanie_nemeth.jpg'),
+	},
+	{
+	  name: 'Eva Ferreira',
+	  title: 'Take on me, web browsers!',
+	  topics: '',
+	  image: require('../assets/speakers/eva_ferreira.jpg'),
+	},
+	{
+	  name: 'Liran Tal',
+	  title: 'StrangerDanger: Finding Security Vulnerabilities Before They Find You!',
+	  topics: '',
+	  image: require('../assets/speakers/liran_tal.jpg'),
+	},
+	{
+	  name: 'Damini Satya Kammakomati',
+	  title: 'Taming “Git”osaurus Using Mystical Trees: Understanding complex git trees in the developer fairyland',
+	  topics: '',
+	  image: require('../assets/speakers/damini_satya.jpg'),
+	},
+	{
+	  name: 'Tejas Kumar',
+	  title: 'Legendary Lambdas',
+	  topics: '',
+	  image: require('../assets/speakers/tejas_kumar.jpg'),
+	},
+	{
+	  name: 'Shelley Vohr',
+	  title: 'API Modernization: Building Bridges As You Cross Them',
+	  topics: '',
+	  image: require('../assets/speakers/shelley_vohr.jpg'),
+	},
+	{
+	  name: 'Vitalii Bobrov',
+	  title: 'Algorithms and Their Habitat',
+	  topics: '',
+	  image: require('../assets/speakers/vitalii_bobrov.jpg'),
+	},
+	{
+	  name: 'Kat Kitay',
+	  title: 'Web Norms of the World: An exploration of the internet beyond the West',
+	  topics: '',
+	  image: require('../assets/speakers/kat_kitay.jpg'),
+	},
+	{
+	  name: 'Benedicte Raae',
+	  title: 'A privacy first period tracker? Is it even possible?',
+	  topics: '',
+	  image: require('../assets/speakers/benedicte_raae.jpg'),
+	},
+	{
+	  name: 'Jorge Marin',
+	  title: 'Testing in production: Ideas, experiences, limits, roadblocks', 
+	  topics: '',
+	  image: require('../assets/speakers/jorge_marin.jpg'),
+	},
+	{
+	  name: 'Taylor Fairbank',
+	  title: 'Javascript Performance in Extreme Conditions: Building WebApps for the Refugee Aid Movement',
+	  topics: '',
+	  image: require('../assets/speakers/taylor_fairbank.jpg'),
+	},
+	{
+	  name: 'Balázs Korossy-Khayll',
+	  title: 'Testing presentation components visually',
+	  topics: '',
+	  image: require('../assets/speakers/balazs_korossy_khayll.jpg'),
+	}
+  ]
+  
+export const cssSpeakers = [
+	{
+	  name: 'David Khourshid',
+	  title: 'Crafting Stateful Styles with State Machines',
+	  topics: '',
+	  image: require('../assets/speakers/david_khourshid.jpg'),
+	},
+	{
+	  name: 'Jeremy Wagner',
+	  title: 'The Weird (yet Practical) World of CSS Paint Worklets',
+	  topics: '',
+	  image: require('../assets/speakers/jeremy_wagner.jpg'),
+	},
+	{
+	  name: 'Giulia Cardieri',
+	  title: 'CSS Games and Drawings in the real world: useful or just fun?',
+	  topics: '',
+	  image: require('../assets/speakers/giulia_cardieri.jpg'),
+	},
+	{
+	  name: 'Damien Senger',
+	  title: 'Variable fonts & readability: the rising of custom reading experiences',
+	  topics: '',
+	  image: require('../assets/speakers/damien_senger.jpg'),
+	},
+	{
+	  name: 'Yu Ling Cheng',
+	  title: 'Building better products faster: DevUx is the new DevOps',
+	  topics: '',
+	  image: require('../assets/speakers/yu_ling_cheng.jpg'),
+	},
+	{
+	  name: 'Mike Riethmuller',
+	  title: 'CSS Architecture for Modern Web Applications',
+	  topics: '',
+	  image: require('../assets/speakers/mike_riethmuller.jpg'),
+	},
+	{
+	  name: 'Xavier Cazalot',
+	  title: 'The Path to SVG Animations',
+	  topics: '',
+	  image: require('../assets/speakers/xavier_cazalot.jpg'),
+	},
+	{
+	  name: 'Kathrin Holzmann',
+	  title: 'CSS in JS - beware the Hype!',
+	  topics: '',
+	  image: require('../assets/speakers/kathrin_holzmann.jpg'),
+	},
+	{
+	  name: 'Alan Stearns',
+	  title: 'Getting Browser Bugs Fixed',
+	  topics: '',
+	  image: require('../assets/speakers/alan_stearns.jpg'),
+	},
+  ]
